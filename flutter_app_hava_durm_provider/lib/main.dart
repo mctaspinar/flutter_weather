38aa50023c94ab5989d1dce660c950e5aba97a72
mete <taspinar92@gmail.com>
2020-11-07 23:49:18 +0300

    test commit

--- flutter_app_hava_durm_provider/lib/main.dart
+++ flutter_app_hava_durm_provider/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hava Durumuuuu',
+      title: 'Hava Durumu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
