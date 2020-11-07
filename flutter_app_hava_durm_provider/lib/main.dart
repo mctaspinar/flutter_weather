77c40bff63218b3ab33e932f1e448ff114cd27f6
Mete Cagri TASPINAR <taspinar92@gmail.com>
2020-11-07 23:40:43 +0300

    denemee"

--- flutter_app_hava_durm_provider/lib/main.dart
+++ flutter_app_hava_durm_provider/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hava Durumuu',
+      title: 'Hava Durumuuu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
